8880b0149b6f76616dc73dff580e7319b6d7a463
Darya Smirnova <dashytka96@bk.ru>
2019-04-07 22:40:19 +0300

    добавлены стартовые файлы веб-приложения
    возможно, этот вариант будет отменен

---
+++ source/web/ScriptOpenWeb.vbs
@@ -0,0 +1,2 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "goServer.bat",0,false
---
+++ source/web/goServer.bat
@@ -0,0 +1,4 @@
+@chcp 65001
+explorer http://localhost:8000/web-страничка.html
+python -m http.server
+pause
